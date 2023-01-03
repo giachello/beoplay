4e6dbc3424a1f7e6bf988466dd4e5eaa7620f9f8
Giovanni Iachello <60585229+giachello@users.noreply.github.com>
2023-01-02 23:05:00 -0800

    Fix next/prev, status update, pybeoplay 1.0.5

--- custom_components/beoplay/media_player.py
+++ custom_components/beoplay/media_player.py
@@ -1,21 +1,17 @@
 """
 Support for Bang & Olufsen speakers
 """
-import asyncio
-from asyncio import CancelledError
 import logging
 from datetime import timedelta
-
-# import aiohttp
-from aiohttp.client_exceptions import ClientError
-
-# from aiohttp.hdrs import CONNECTION, KEEP_ALIVE
-# import async_timeout
 import voluptuous as vol
 
-import json
+import asyncio
+from asyncio import CancelledError
+from aiohttp import ServerDisconnectedError
+from aiohttp.client_exceptions import ClientError
 
-import pybeoplay as beoplay
+
+import pybeoplay
 
 from .const import (
     DOMAIN,
@@ -28,7 +24,6 @@ from homeassistant.helpers.typing import ServiceDataType
 from homeassistant.helpers.entity import DeviceInfo, generate_entity_id
 from homeassistant.components.media_player import MediaPlayerEntity
 from homeassistant.components.media_player.const import (
-    DOMAIN,
     MEDIA_TYPE_MUSIC,
     SUPPORT_NEXT_TRACK,
     SUPPORT_PAUSE,
@@ -41,9 +36,11 @@ from homeassistant.components.media_player.const import (
     SUPPORT_STOP,
     SUPPORT_PLAY,
     SUPPORT_SELECT_SOURCE,
+    RepeatMode,
 )
 from homeassistant.const import (
     ATTR_ENTITY_ID,
+    CONF_URL,
     CONF_HOST,
     STATE_OFF,
     STATE_ON,
@@ -67,10 +64,10 @@ REQUIREMENTS = ["pybeoplay"]
 _LOGGER = logging.getLogger(__name__)
 
 MIN_TIME_BETWEEN_UPDATES = timedelta(
-    seconds=10
+    seconds=20
 )  # need this to check on the power state, that doesnt come in the stream of notifications
 
-CHECK_TIMEOUT = 10
+CHECK_TIMEOUT = 5
 
 SUPPORT_BEOPLAY = (
     SUPPORT_PAUSE
@@ -90,12 +87,21 @@ DATA_BEOPLAY = "beoplay_media_player"
 
 BEOPLAY_EXPERIENCE_JOIN_SERVICE = "beoplay_join_experience"
 BEOPLAY_EXPERIENCE_LEAVE_SERVICE = "beoplay_leave_experience"
+BEOPLAY_ADD_MEDIA_SERVICE = "beoplay_add_media_to_queue"
 
 EXPERIENCE_SCHEMA = vol.Schema(
     {
         vol.Required(ATTR_ENTITY_ID): cv.entity_ids,
     }
 )
+
+ADD_MEDIA_SCHEMA = vol.Schema(
+    {
+        vol.Required(ATTR_ENTITY_ID): cv.entity_ids,
+        vol.Required(CONF_URL): cv.url,
+    }
+)
+
 
 ENTITY_ID_FORMAT = DOMAIN + ".{}"
 
@@ -107,7 +113,7 @@ class BeoPlayData:
         self.entities = []
 
 
-def _add_player(hass, async_add_devices, host):
+async def _add_player(hass, async_add_devices, host):
     """Add speakers."""
 
     @callback
@@ -122,12 +128,12 @@ def _add_player(hass, async_add_devices, host):
 
     hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STOP, _stop_polling)
 
-    beoplayapi = beoplay.BeoPlay(host)
-    speaker = BeoPlay(hass, beoplayapi)
+    speaker = BeoPlay(hass, host)
+    await speaker.async_update()
     async_add_devices([speaker], True)
     #        async_add_entities(sensors, update_before_add=True)
 
-    _LOGGER.info("Added device with name: %s", speaker._name)
+    _LOGGER.info("Added device with name: %s", speaker.name)
 
     if hass.is_running:
         _start_polling()
@@ -167,6 +173,18 @@ async def async_setup_entry(
         for entity in entities:
             entity.leave_experience()
 
+    def add_media(service: ServiceDataType):
+        """Leave an existing experience"""
+        _LOGGER.debug("Add Media to Queue service called")
+        entity_ids = service.data.get("entity_id")
+        url = service.data.get("url")
+        entities = hass.data[DATA_BEOPLAY].entities
+
+        if entity_ids:
+            entities = [e for e in entities if e.entity_id in entity_ids]
+        for entity in entities:
+            entity.add_media(url)
+
     host = config_entry.data[CONF_HOST]
 
     if DATA_BEOPLAY not in hass.data:
@@ -184,8 +202,13 @@ async def async_setup_entry(
         leave_experience,
         schema=EXPERIENCE_SCHEMA,
     )
-
-    _add_player(hass, async_add_entities, host)
+    hass.services.async_register(
+        DOMAIN,
+        BEOPLAY_ADD_MEDIA_SERVICE,
+        add_media,
+        schema=ADD_MEDIA_SCHEMA,
+    )
+    await _add_player(hass, async_add_entities, host)
 
 
 async def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
@@ -213,6 +236,18 @@ async def async_setup_platform(hass, config, async_add_devices, discovery_info=N
         for entity in entities:
             entity.leave_experience()
 
+    def add_media(service: ServiceDataType):
+        """Leave an existing experience"""
+        _LOGGER.debug("Add Media to Queue service called")
+        entity_ids = service.data.get("entity_id")
+        url = service.data.get("url")
+        entities = hass.data[DATA_BEOPLAY].entities
+
+        if entity_ids:
+            entities = [e for e in entities if e.entity_id in entity_ids]
+        for entity in entities:
+            entity.add_media(url)
+
     host = config.get(CONF_HOST)
 
     if DATA_BEOPLAY not in hass.data:
@@ -230,37 +265,36 @@ async def async_setup_platform(hass, config, async_add_devices, discovery_info=N
         leave_experience,
         schema=EXPERIENCE_SCHEMA,
     )
+    hass.services.async_register(
+        DOMAIN,
+        BEOPLAY_ADD_MEDIA_SERVICE,
+        add_media,
+        schema=ADD_MEDIA_SCHEMA,
+    )
 
-    _add_player(hass, async_add_devices, host)
+    await _add_player(hass, async_add_devices, host)
 
 
 class BeoPlay(MediaPlayerEntity):
     """Representation of a BeoPlay speaker"""
 
-    def __init__(self, hass, speaker):
+    def __init__(self, hass, host):
         self._hass: HomeAssistant = hass
         self._polling_session = async_get_clientsession(hass)
         self._polling_task = None  # The actual polling task.
         self._first_run = True
 
-        self._speaker = speaker
-        self._state = self._speaker.state
-        self._min_volume = self._speaker.min_volume
-        self._max_voume = self._speaker.max_volume
-        self._volume = self._speaker.volume
-        self._muted = self._speaker.muted
-        self._source = self._speaker.source
-        self._sources = self._speaker.sources
-        self._media_url = self._speaker.media_url
-        self._media_album = self._speaker.media_album
-        self._media_track = self._speaker.media_track
-        self._media_artist = self._speaker.media_artist
+        # this is the connection manager with the actual speaker/TV
+        self._speaker = pybeoplay.BeoPlay(host, self._polling_session)
         self._connfail = 0
+
         self._serial_number = ""
         self._name = ""
-        self._model = ""
-        self._on = self._speaker.on
+        self._type_number = ""
+        self._item_number = ""
         self._unique_id = ""
+        self._on = self._speaker.on
+        self._state = self._speaker.state
 
     async def async_added_to_hass(self):
         self.hass.data[DATA_BEOPLAY].entities.append(self)
@@ -276,7 +310,7 @@ class BeoPlay(MediaPlayerEntity):
 
         except (asyncio.TimeoutError, ClientError, BeoPlay._TimeoutException):
             _LOGGER.info("Node %s is offline, retrying later", self._name)
-            await asyncio.sleep(CHECK_TIMEOUT, loop=self._hass.loop)
+            await asyncio.sleep(CHECK_TIMEOUT)
             self.start_polling()
 
         except CancelledError:
@@ -296,57 +330,19 @@ class BeoPlay(MediaPlayerEntity):
     async def async_update_status(self):
         """Long polling task"""
 
-        try:
-            response = await self._polling_session.request(
-                "get", self._speaker._host_notifications
-            )
-
-            if response.status == 200:
-                while True:
-                    data = await response.content.readline()
-                    if not data:
-                        break
-                    data = data.decode("utf-8").replace("\r", "").replace("\n", "")
-                    if data:
-                        _LOGGER.info("Update status: " + self._name + data)
-
-                        data_json = json.loads(data)
-                        ############################################################
-                        # functions are coming here that update the properties
-                        ############################################################
-                        # get volume
-                        self._speaker.getVolume(data_json)
-                        # get source
-                        self._speaker.getSource(data_json)
-                        # get state
-                        self._speaker.getState(data_json)
-                        # get currently playing music info
-                        self._speaker.getMusicInfo(data_json)
-
-                        self._volume = self._speaker.volume
-                        self._muted = self._speaker.muted
-                        self._state = self._speaker.state
-                        self._on = self._speaker.on
-                        self._source = self._speaker.source
-                        self._media_url = self._speaker.media_url
-                        self._media_album = self._speaker.media_album
-                        self._media_track = self._speaker.media_track
-                        self._media_artist = self._speaker.media_artist
-
-                        self.async_schedule_update_ha_state()
-                        self._hass.add_job(
-                            self._notify_beoplay_notification, data_json["notification"]
-                        )
-            else:
-                _LOGGER.error(
-                    "Error %s on %s. Trying one more time",
-                    response.status,
-                    self._speaker._host_notifications,
-                )
-
-        except (asyncio.TimeoutError, ClientError):
+        def notif_callback(data: dict):
+            self._on = self._speaker.on
+            self._state = self._speaker.state
             self.async_schedule_update_ha_state()
-            _LOGGER.info("Client connection error, marking %s as offline", self._name)
+            self._hass.add_job(self._notify_beoplay_notification, data)
+
+        try:
+            await self._speaker.async_notificationsTask(notif_callback)
+        except (asyncio.TimeoutError, ClientError) as _e:
+            # occasionally the notifications stream is closed by the speaker/TV
+            # In that case, restart the polling
+            self.async_schedule_update_ha_state()
+            _LOGGER.info("Client error %s on %s" % (str(_e), self._name))
             raise
 
     # ========== Events ==============
@@ -373,7 +369,7 @@ class BeoPlay(MediaPlayerEntity):
             name=self._name,
             manufacturer="Bang & Olufsen",
             via_device=(DOMAIN, self._serial_number),
-            model=self._model,
+            model=self._type_number,
         )
 
     @property
@@ -408,22 +404,22 @@ class BeoPlay(MediaPlayerEntity):
     @property
     def source(self):
         """Return the current input source."""
-        return self._source
+        return self._speaker.source if self._speaker.source else None
 
     @property
     def source_list(self):
         """List of available input sources."""
-        return self._sources if self._sources else None
+        return self._speaker.sources if self._speaker.sources else None
 
     @property
     def volume_level(self):
         """Volume level of the media player (0..1)."""
-        return self._volume
+        return self._speaker.volume
 
     @property
     def is_volume_muted(self):
         """Boolean if volume is currently muted."""
-        return self._muted
+        return self._speaker.muted
 
     @property
     def media_content_type(self):
@@ -433,37 +429,37 @@ class BeoPlay(MediaPlayerEntity):
     @property
     def media_image_url(self):
         """Image url of current playing media."""
-        if self._source == "AirPlay":
-            return None
-        return self._media_url if self._media_url else None
+        # if self._speaker.source == "AirPlay":
+        #    return None
+        return self._speaker.media_url if self._speaker.media_url else None
 
     @property
     def media_title(self):
         """Title of current playing media."""
-        if self._source == "Google Cast":
-            return self._media_album
+        if self._speaker.source == "Google Cast":
+            return self._speaker.media_album
         # return self._speaker.source if self._speaker.source else None
-        return self._media_track if self._media_track else None
+        return self._speaker.media_track if self._speaker.media_track else None
 
     @property
     def media_track(self):
         """Track number of current playing media (Music track only)."""
-        return self._media_track if self._media_track else None
+        return self._speaker.media_track if self._speaker.media_track else None
 
     @property
     def media_artist(self):
         """Artist of current playing media (Music track only)."""
-        return self._media_artist if self._media_artist else None
+        return self._speaker.media_artist if self._speaker.media_artist else None
 
     @property
     def media_album(self):
         """Album of current playing media (Music track only)."""
-        return self._media_album if self._media_album else None
+        return self._speaker.media_album if self._speaker.media_album else None
 
     @property
     def app_name(self):
         """Name of the current running app."""
-        return self._source if self._source else None
+        return self.source
 
     # ========== Service Calls ==========
 
@@ -495,18 +491,21 @@ class BeoPlay(MediaPlayerEntity):
         """Send next track command."""
         self._speaker.Next()
 
+    def set_shuffle(self, shuffle: bool) -> None:
+        """Send previous track command."""
+        self._speaker.Prev()
+
+    def set_repeat(self, repeat: RepeatMode) -> None:
+        """Send next track command."""
+        self._speaker.Next()
+
     def set_volume_level(self, volume):
         """Set volume level, range 0..1."""
         self._speaker.setVolume(volume)
 
     def mute_volume(self, mute):
         """Send mute command"""
-        if self._muted:
-            self._speaker.setMute(False)
-            self._muted = False
-        else:
-            self._speaker.setMute(True)
-            self._muted = True
+        self._speaker.setMute(mute)
 
     def select_source(self, source):
         """Select input source."""
@@ -520,25 +519,35 @@ class BeoPlay(MediaPlayerEntity):
         """Leave experience"""
         self._speaker.leaveExperience()
 
+    def add_media(self, url):
+        """Leave experience"""
+        item = {
+            "playQueueItem": {"behaviour": "impulsive", "track": {"dlna": {"url": url}}}
+        }
+        self._speaker.playQueueItem(False, item)
+
     @Throttle(MIN_TIME_BETWEEN_UPDATES)
-    def update(self):
+    async def async_update(self):
         """Get the latest data and update device state."""
         # _LOGGER.debug("Updating")
-        self._speaker.getStandby()
-        if self._on != self._speaker.on:
-            self._on = self._speaker.on
-            _LOGGER.debug("Updating ON state: %s", self._on)
+
         if self._first_run:
-            self._speaker.getSources()
-            self._sources = self._speaker.sources
-            ## get the information about the device
-            _r = self._speaker._getReq("BeoDevice")
-            if _r:
-                self._serial_number = _r["beoDevice"]["productId"]["serialNumber"]
-                self._name = _r["beoDevice"]["productFriendlyName"][
-                    "productFriendlyName"
-                ]
-                self._model = _r["beoDevice"]["productId"]["typeNumber"]
-                self._unique_id = f"beoplay-{self._serial_number}-media_player"
-            self.entity_id = generate_entity_id(ENTITY_ID_FORMAT, self._serial_number, hass=self._hass)
+            await self._speaker.async_get_device_info()
+            #            self._sources = self._speaker.sources
+            self._serial_number = self._speaker.serialNumber
+            self._name = self._speaker.name
+            self._type_number = self._speaker.typeNumber
+            self._item_number = self._speaker.itemNumber
+            self._unique_id = f"beoplay-{self._serial_number}-media_player"
+            self.entity_id = generate_entity_id(
+                ENTITY_ID_FORMAT, self._serial_number, hass=self._hass
+            )
+            await self._speaker.async_get_sources()
             self._first_run = False
+        try:
+            await self._speaker.async_get_standby()
+            if self._on != self._speaker.on:
+                self._on = self._speaker.on
+                _LOGGER.debug("Updating ON state: %s", self._on)
+        except ServerDisconnectedError:
+            _LOGGER.debug("Server disconnected, ignoring")
